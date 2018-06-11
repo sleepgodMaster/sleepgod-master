81b9fc2191cb9127173a920ecd2cb90dc3c27a7c
[fuwei] <[wei.fu01@hand-china.com]>
2018-06-11 16:38:10 +0800

    update .gitignore

---
+++ samples/src/main/java/com/sleepgod/sleepgod/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sleepgod.sleepgod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
